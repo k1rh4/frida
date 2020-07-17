1850f011403cdb6e61b3eeac1fe4b05ddb85879b
k1rh4 <k1rha87@gmail.com>
2020-07-17 10:42:35 +0900

    Load Class and method script

---
+++ loadAllClass.js
@@ -0,0 +1,91 @@
+// Script Inspired by https://github.com/0xdea/frida-scripts/tree/master/android-snippets
+function enumAllClasses() {
+    var allClasses = [];
+    var classes = Java.enumerateLoadedClassesSync();
+
+    classes.forEach(function(aClass) {
+        try {
+            var className = aClass.replace(/\//g, ".");
+        } catch (err) {}
+        allClasses.push(className);
+    });
+
+    return allClasses;
+}
+
+function enumClassLoaders(){
+    var allClassLoaders = []
+    var classLoaders = Java.enumerateClassLoadersSync()
+
+    classLoaders.forEach(function(cl) {
+        allClassLoaders.push(cl);
+    });
+
+    return allClassLoaders;
+}
+
+function enumDexClasses(apk_path) {
+    var BaseDexClassLoader = Java.use("dalvik.system.BaseDexClassLoader");
+    var DexFile = Java.use("dalvik.system.DexFile");
+    var df = DexFile.$new(apk_path);
+    var en = df.entries()
+
+    var dexClasses = []
+    while(en.hasMoreElements()){
+        dexClasses.push(en.nextElement());
+    }
+
+    return dexClasses;
+}
+
+function findClasses(pattern) {
+    var allClasses = enumAllClasses();
+    var foundClasses = [];
+
+    allClasses.forEach(function(aClass) {
+        try {
+            if (aClass.match(pattern)) {
+                foundClasses.push(aClass);
+            }
+        } catch (err) {}
+    });
+
+    return foundClasses;
+}
+
+function enumMethods(targetClass) {
+    var hook = Java.use(targetClass);
+    var ownMethods = hook.class.getDeclaredMethods();
+    hook.$dispose;
+
+    return ownMethods;
+}
+
+function enumLibSo(lib_name){
+    exports = Module.enumerateExportsSync(lib_name);
+    var foundObj = []
+    for(i=0; i<exports.length; i++){
+        foundObj.push(String(exports[i].name) + " : " + String(exports[i].address))
+    }
+    return foundObj
+}
+
+setTimeout(function() {
+    Java.perform(function() {
+        var sendback = ''
+        var classname_return = ''
+        var methods_return = ''
+        var enum_signature = '-enumMmMmMmMm-'
+       
+        
+        // enumerate all classes
+	    var a = enumAllClasses();
+	    a.forEach(function(s) {
+            if (s){classname_return += JSON.stringify(s) + '\n'}
+	    });
+        sendback = enum_signature + classname_return
+
+        
+        send(sendback)
+    });
+}, 0);
